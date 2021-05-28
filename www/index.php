528551e5be892eca848720ea3249fee2bd11b282
dd32 <dd32@theme-check.theme-check.run>
2021-05-28 18:52:12 +1000

    Make the theme url wide

--- www/index.php
+++ www/index.php
@@ -4,17 +4,17 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700">
-	<link rel="stylesheet" href="/style.css">
+	<link rel="stylesheet" href="/style.css?1">
 </head>
 <body>
 	<div id="main">
 		<form id="test" action="/create.php" method="POST">
 			<label for="theme">Theme:</label>
 			<div id="createFields">
-				<input name="theme" type="text" required value="https://themes.svn.wordpress.org/twentyten/3.3/">
+				<input name="theme" id="theme" type="text" required value="https://themes.svn.wordpress.org/twentyten/3.3/">
 				<input type="submit" value="Test">
 			</div>
-			<p>For now, the only supported input is a full Themes.svn path.</p>
+			<p>For now, the only supported input is a full SVN URL.</p>
 		</form>
 	</div>
 </body>
