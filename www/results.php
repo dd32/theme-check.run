673514ca616d1427d68a58db0887d6e0472e4efa
dd32 <dd32@theme-check.theme-check.run>
2021-06-02 13:21:32 +1000

    Add a restart link to finished jobs

--- www/results.php
+++ www/results.php
@@ -13,6 +13,14 @@ if ( ! $id || ! $run ) {
 	exit;
 }
 
+if ( !empty( $_GET['restart'] ) ) {
+	header( 'Location: /create.php?' . http_build_query( json_decode( $run['meta'] ) ) );
+	exit;
+}
+
+if ( 'finished' === $run['status'] ) {
+	echo '<a style="float: right; padding: 1em;" href="?id=' . $id . '&restart=please">Restart</a>';
+}
 
 printf( '<strong>Requested:</strong> %s<br>', $run['time'] );
 printf( '<strong>Status:</strong> %s<br>', $run['status'] );
