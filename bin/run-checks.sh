c57e335a417e8feaa9051f2279e4be22bd391bbf
Paulo Pinto <paulo.pinto@automattic.com>
2021-08-04 15:27:13 +0100

    Add comments

--- bin/run-checks.sh
+++ bin/run-checks.sh
@@ -37,8 +37,10 @@ cd "$WORKING_DIR/theme-review-action" || exit
 git pull
 git checkout run_themechecks_against_theme
 
+# Install theme-review-action dependencies.
 { npm install > /dev/null; } 2>&1
 
+# Run theme-review-action.
 PORT=$( shuf -i 5000-65000 -n 1 )
 # npm run start --skipFolderCopy --pathToTheme=../test-theme --port $PORT
 node bin/program.js --skipFolderCopy --pathToTheme=../test-theme --port $PORT
