32a709d16bb966bc74097697ccad73f8da3aa984
Paulo Pinto <paulo.pinto@automattic.com>
2021-08-04 15:29:45 +0100

    Fix formatting

--- bin/run-checks.sh
+++ bin/run-checks.sh
@@ -5,13 +5,13 @@ cd "$(dirname "$(dirname "$0")")" || exit
 
 ID=$1
 if [ -z "$ID" ]; then
-	echo No hash provided
-	exit 1
+  echo No hash provided
+  exit 1
 fi
 SVN=$2
 if [ -z "$SVN" ]; then
-	echo No SVN directory provided
-	exit 1
+  echo No SVN directory provided
+  exit 1
 fi
 
 echo "Running job $ID"
@@ -38,9 +38,8 @@ git pull
 git checkout run_themechecks_against_theme
 
 # Install theme-review-action dependencies.
-{ npm install > /dev/null; } 2>&1
+{ npm install >/dev/null; } 2>&1
 
 # Run theme-review-action.
-PORT=$( shuf -i 5000-65000 -n 1 )
-# npm run start --skipFolderCopy --pathToTheme=../test-theme --port $PORT
-node bin/program.js --skipFolderCopy --pathToTheme=../test-theme --port $PORT
+PORT=$(shuf -i 5000-65000 -n 1)
+node bin/program.js --skipFolderCopy --pathToTheme=../test-theme --port "$PORT"
